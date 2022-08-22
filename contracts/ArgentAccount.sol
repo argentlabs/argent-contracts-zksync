93783d05f31318ff467c6b5e5a91f7f16bd76d85
Axel Delamarre <1091900+delaaxe@users.noreply.github.com>
2022-08-22 14:57:18 +0200

    Pin solidity versions

--- contracts/ArgentAccount.sol
+++ contracts/ArgentAccount.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.10;
+pragma solidity 0.8.15;
 
 import {BOOTLOADER_FORMAL_ADDRESS, NONCE_HOLDER_SYSTEM_CONTRACT} from "@matterlabs/zksync-contracts/l2/system-contracts/Constants.sol";
 import {Transaction, TransactionHelper} from "@matterlabs/zksync-contracts/l2/system-contracts/TransactionHelper.sol";
--- contracts/Proxy.sol
+++ contracts/Proxy.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.10;
+pragma solidity 0.8.15;
 
 contract Proxy {
     address public immutable implementation;
