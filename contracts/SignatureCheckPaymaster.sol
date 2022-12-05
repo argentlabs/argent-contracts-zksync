7de825443427790e8507280f5173f8dea1c9d131
Axel Delamarre <1091900+delaaxe@users.noreply.github.com>
2022-12-05 15:14:42 +0200

    Use typed data encoding in contract

--- contracts/SignatureCheckPaymaster.sol
+++ contracts/SignatureCheckPaymaster.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.16;
 
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 import {ERC165Checker} from "@openzeppelin/contracts/utils/introspection/ERC165Checker.sol";
 import {IERC1271} from "@openzeppelin/contracts/interfaces/IERC1271.sol";
 import {Transaction, TransactionHelper} from "@matterlabs/zksync-contracts/l2/system-contracts/TransactionHelper.sol";
@@ -30,29 +31,29 @@ library MeaningfulTransaction {
     }
 }
 
-contract SignatureCheckPaymaster is SponsorPaymaster {
+contract SignatureCheckPaymaster is SponsorPaymaster, EIP712 {
     using TransactionHelper for Transaction;
     using MeaningfulTransaction for Transaction;
     using ERC165Checker for address;
     using ECDSA for bytes32;
 
-    constructor() {
-        requireIERC1271Support(owner);
+    constructor() EIP712("SignatureCheckPaymaster", "1") {
+        requireERC1271Support(owner);
     }
 
     function isSponsoredTransaction(Transaction calldata _transaction) internal view override returns (bool) {
-        bytes32 messageHash = _transaction.hashMeaningfulTransaction().toEthSignedMessageHash();
         bytes memory signature = abi.decode(_transaction.paymasterInput[4:], (bytes));
+        bytes32 messageHash = _hashTypedDataV4(_transaction.hashMeaningfulTransaction());
         bytes4 result = IERC1271(owner).isValidSignature(messageHash, signature);
         return result == IERC1271.isValidSignature.selector;
     }
 
     function _changeOwner(address _newOwner) internal override onlyOwner {
-        requireIERC1271Support(_newOwner);
+        requireERC1271Support(_newOwner);
         super._changeOwner(_newOwner);
     }
 
-    function requireIERC1271Support(address _owner) internal view {
-        require(_owner.supportsInterface(type(IERC1271).interfaceId), "non-IERC1271 owner");
+    function requireERC1271Support(address _owner) internal view {
+        require(_owner.supportsInterface(type(IERC1271).interfaceId), "non-ERC1271 owner");
     }
 }
