02c3fe86079ec377fbae1d72c7732a1bf5a03c08
Axel Delamarre <1091900+delaaxe@users.noreply.github.com>
2022-12-07 18:44:26 +0200

    Update EOASignatureCheckPaymaster.sol

--- contracts/EOASignatureCheckPaymaster.sol
+++ contracts/EOASignatureCheckPaymaster.sol
@@ -2,20 +2,18 @@
 pragma solidity 0.8.16;
 
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import {SignatureChecker} from "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
 import {Transaction} from "@matterlabs/zksync-contracts/l2/system-contracts/TransactionHelper.sol";
 
 import {SponsorPaymaster} from "./SponsorPaymaster.sol";
 import {MeaningfulTransaction} from "./SignatureCheckPaymaster.sol";
 
-contract EOASignatureCheckPaymaster is SponsorPaymaster {
+contract EOASignatureCheckPaymaster is SponsorPaymaster, EIP712 {
     using MeaningfulTransaction for Transaction;
-    using SignatureChecker for address;
     using ECDSA for bytes32;
 
     function isSponsoredTransaction(Transaction calldata _transaction) internal view override returns (bool) {
-        bytes32 messageHash = _transaction.hashMeaningfulTransaction().toEthSignedMessageHash();
         bytes memory signature = abi.decode(_transaction.paymasterInput[4:], (bytes));
+        bytes32 messageHash = _hashTypedDataV4(_transaction.hashMeaningfulTransaction());
         return owner.isValidSignatureNow(messageHash, signature);
     }
 }
