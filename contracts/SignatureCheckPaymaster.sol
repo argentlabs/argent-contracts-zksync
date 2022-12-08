ee4a1a5527b99bc1481458c38ecfb6513e7d54b3
Axel Delamarre <1091900+delaaxe@users.noreply.github.com>
2022-12-08 12:50:15 +0200

    Revert to eth signed message

--- contracts/SignatureCheckPaymaster.sol
+++ contracts/SignatureCheckPaymaster.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.16;
 
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 import {ERC165Checker} from "@openzeppelin/contracts/utils/introspection/ERC165Checker.sol";
 import {IERC1271} from "@openzeppelin/contracts/interfaces/IERC1271.sol";
 import {Transaction, TransactionHelper} from "@matterlabs/zksync-contracts/l2/system-contracts/TransactionHelper.sol";
@@ -31,19 +30,19 @@ library MeaningfulTransaction {
     }
 }
 
-contract SignatureCheckPaymaster is SponsorPaymaster, EIP712 {
+contract SignatureCheckPaymaster is SponsorPaymaster {
     using TransactionHelper for Transaction;
     using MeaningfulTransaction for Transaction;
     using ERC165Checker for address;
     using ECDSA for bytes32;
 
-    constructor() EIP712("SignatureCheckPaymaster", "1") {
+    constructor() {
         requireERC1271Support(owner);
     }
 
     function isSponsoredTransaction(Transaction calldata _transaction) internal view override returns (bool) {
         bytes memory signature = abi.decode(_transaction.paymasterInput[4:], (bytes));
-        bytes32 messageHash = _hashTypedDataV4(_transaction.hashMeaningfulTransaction());
+        bytes32 messageHash = _transaction.hashMeaningfulTransaction().toEthSignedMessageHash();
         bytes4 result = IERC1271(owner).isValidSignature(messageHash, signature);
         return result == IERC1271.isValidSignature.selector;
     }
