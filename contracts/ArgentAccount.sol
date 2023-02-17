9266a826ec652186d3b0bbfdb41ca842aab80f22
Axel Delamarre <1091900+delaaxe@users.noreply.github.com>
2023-02-17 15:00:21 +0100

    Add alpha tag to version

--- contracts/ArgentAccount.sol
+++ contracts/ArgentAccount.sol
@@ -36,7 +36,7 @@ contract ArgentAccount is IAccount, IERC165, IERC1271 {
         bytes data;
     }
 
-    bytes32 public constant VERSION = bytes32(abi.encodePacked("0.0.1"));
+    bytes32 public constant VERSION = bytes32(abi.encodePacked("0.0.1-alpha.1"));
 
     uint8 public constant NO_ESCAPE = uint8(EscapeType.None);
     uint8 public constant GUARDIAN_ESCAPE = uint8(EscapeType.Guardian);
