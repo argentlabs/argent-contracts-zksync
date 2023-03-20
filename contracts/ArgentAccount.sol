116d6804c135299d48c0545d7305578fecb47f2a
delaaxe <1091900+delaaxe@users.noreply.github.com>
2023-03-20 23:12:38 +0100

    Add signature to `changeOwner`

--- contracts/ArgentAccount.sol
+++ contracts/ArgentAccount.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.18;
 import {IERC165} from "@openzeppelin/contracts/interfaces/IERC165.sol";
 import {IERC1271} from "@openzeppelin/contracts/interfaces/IERC1271.sol";
 import {ERC165Checker} from "@openzeppelin/contracts/utils/introspection/ERC165Checker.sol";
+import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 import {BOOTLOADER_FORMAL_ADDRESS, DEPLOYER_SYSTEM_CONTRACT, NONCE_HOLDER_SYSTEM_CONTRACT} from "@matterlabs/zksync-contracts/l2/system-contracts/Constants.sol";
 import {IAccount, ACCOUNT_VALIDATION_SUCCESS_MAGIC} from "@matterlabs/zksync-contracts/l2/system-contracts/interfaces/IAccount.sol";
@@ -20,6 +21,7 @@ import {Signatures} from "./Signatures.sol";
 contract ArgentAccount is IAccount, IProxy, IMulticall, IERC165, IERC1271 {
     using TransactionHelper for Transaction;
     using ERC165Checker for address;
+    using ECDSA for bytes32;
 
     struct Version {
         uint8 major;
@@ -171,9 +173,11 @@ contract ArgentAccount is IAccount, IProxy, IMulticall, IERC165, IERC1271 {
 
     /**************************************************** Recovery ****************************************************/
 
-    function changeOwner(address _newOwner) external {
+    function changeOwner(address _newOwner, bytes memory _signature) external {
         requireOnlySelf();
         require(_newOwner != address(0), "argent/null-owner");
+        requireValidNewOwnerSignature(_newOwner, _signature);
+
         owner = _newOwner;
         emit OwnerChanged(_newOwner);
     }
@@ -437,6 +441,14 @@ contract ArgentAccount is IAccount, IProxy, IMulticall, IERC165, IERC1271 {
     }
 
     /**************************************************** Recovery ****************************************************/
+
+    function requireValidNewOwnerSignature(address _newOwner, bytes memory _signature) internal view {
+        bytes4 selector = this.changeOwner.selector;
+        bytes memory message = abi.encodePacked(selector, block.chainid, address(this), owner);
+        bytes32 messageHash = keccak256(message).toEthSignedMessageHash();
+        address signer = Signatures.recoverSigner(messageHash, _signature);
+        require(signer != address(0) && signer == _newOwner, "argent/invalid-owner-sig");
+    }
 
     function isOwnerEscapeCall(bytes4 _selector) internal pure returns (bool) {
         return _selector == this.escapeOwner.selector || _selector == this.triggerEscapeOwner.selector;
