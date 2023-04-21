8f88a8c6251704e52433c84f22cdbe67adac0d95
delaaxe <1091900+delaaxe@users.noreply.github.com>
2023-04-21 10:18:40 +0200

    Update ArgentAccount.sol

--- contracts/ArgentAccount.sol
+++ contracts/ArgentAccount.sol
@@ -211,12 +211,11 @@ contract ArgentAccount is IAccount, IProxy, IMulticall, IERC165, IERC1271 {
 
     /// @dev Logic to execute after an upgrade.
     /// Can only be called by the account after a call to `upgrade`.
-    /// @param _oldImplementation Address of the previous account implementation
-    /// @param _data Generic call data that can be passed to the method for future upgrade logic
+    //  @param _oldImplementation Address of the previous account implementation
+    //  @param _data Generic call data that can be passed to the method for future upgrade logic
     function executeAfterUpgrade(address /*_oldImplementation*/, bytes calldata /*_data*/) external {
         _requireOnlySelf();
         owner = owner; // useless code to suppress warning about pure function
-        // reserved upgrade callback for future account versions
     }
 
     /// @inheritdoc IAccount
